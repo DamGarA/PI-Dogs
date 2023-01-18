bc0a6ffb73feb759f2fd614dc245b141aa20cfe0
DamGarA <damiangarciaabreu@gmail.com>
2023-01-18 05:35:57 -0300

    18-1 5am

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,15 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { useDispatch, useSelector } from 'react-redux'
 import { addAllDogs } from "../redux/actions"
+import Pagination from "./pagination"
+import styled from "styled-components"
 
  function Home() {
+    const [page, setPage] = useState(1)
+    const [amountPerPage, setAmountPerPage] = useState(8)
     const dispatch = useDispatch()
     const actualState = useSelector(state => state.allDogs)
+    const max = Math.ceil(actualState.length / amountPerPage) 
 
     useEffect(() => {
         fetch('http://localhost:3001/dogs')
@@ -17,12 +22,42 @@ import { addAllDogs } from "../redux/actions"
     return (
         <div>
             <h1>Home</h1>
-            {actualState?.map(race => (
-                <p>{race.name}</p>
+            <Pagination page={page} setPage={setPage} max={max}/>
+            {actualState
+                ?.slice((page - 1) * amountPerPage, (page - 1) * amountPerPage + amountPerPage)
+                .map(race => (
+                <RaceDiv>
+                <RaceContent>
+                <EseP>{race.name}</EseP>
+                <RaceImg src={race.image}></RaceImg>
+                </RaceContent>
+                </RaceDiv>
             ))}
         </div>
         
     )
 }
+
+const RaceDiv = styled.div`
+    display: flex;
+    float: left;
+    justify-content: space-around
+`
+
+const RaceContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+
+const RaceImg = styled.img`
+    max-height:100px;
+    max-width: 100px;
+`
+
+const EseP = styled.p`
+    
+`
 
 export default Home
---
+++ client/src/components/pagination.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Pagination({page, setPage, max}) {
+    console.log(max)
+    return (
+        <div>
+            <p>jpja</p>
+        </div>
+    )
+}
+
+export default Pagination
